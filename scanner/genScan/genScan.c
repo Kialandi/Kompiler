1ff9a04f1c71eeb4f5a7cc03a6b27845cd68bb3f
Kialandi <sam-yap@hotmail.com>
2018-02-19 04:19:57 -0500

    re-did stMachine

--- scanner/genScan/genScan.c
+++ scanner/genScan/genScan.c
@@ -13,16 +13,16 @@ int main (int arg, char ** argv) {
             printf(",");
 
         printf("{ /* State row %d */\n", i);
-        printf("\t/* JUNK */ {0, &errFunc, 0},\n");
+        printf("\t/* JUNK */ {ERRST, &errFunc, 0},\n");
         printf("\t/* WHITESPACE */ {0, &errFunc, 0},\n");
         printf("\t/* NUMBERS */ {NUMBERST, &accumulate, 0},\n");
         printf("\t/* CHARS */ {IDST, &accumulate, 0},\n");
         
         for (j = 0; j < ASCIIMAX + 1; j++) {
             if ((j > ' ' && j < '0') || (j > '9' && j < 'A') || (j > 'Z' && j < 'a') || (j > 'z' && j < 126))
-                printf("\t/* %c */ {%d, &errFunc, 0},\n", j, 0);
+                printf("\t/* %c */ {ERRST, &errFunc, 0},\n", j);
             else if (j == 126)
-                printf("\t/* %c */ {%d, &errFunc, 0}\n", j, 0);
+                printf("\t/* %c */ {ERRST, &errFunc, 0}\n", j);
         
         }
         printf("}\n\n");
